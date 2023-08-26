7d8c31a0b6638fd6fcdc560a8b0d8231d17a7632
ash81166 <aishikchatterjee12@gmail.com>
2023-08-26 14:03:31 +0530

    hyperparams tuning and Flask app 1.0

--- src/utils.py
+++ src/utils.py
@@ -17,13 +17,22 @@ def save_obj(file_path, obj):
             dill.dump(obj,file_obj)
     except Exception as e:
         raise Custom_Exception(e,sys)
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
             y_train_pred = model.predict(X_train)
 
             y_test_pred = model.predict(X_test)
